c43239069788dab41c58dbdc7f30a15eacfddae9
Your Name <you@example.com>
2023-11-14 18:00:52 +0300

    loadfour

--- app/js/index.js
+++ app/js/index.js
@@ -6,7 +6,10 @@ import '../scss/index.scss';
 
 //js
 import $ from 'jquery';
-import Swiper from 'swiper';
+import Swiper,{ Navigation, Pagination } from 'swiper';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 import { Fancybox, Carousel, Panzoom } from '@fancyapps/ui';
 import IMask from 'imask';
 import Choices from 'choices.js';
@@ -18,3 +21,25 @@ import noUiSlider from 'nouislider';
 import Scrollbar from 'smooth-scrollbar';
 
 //// ================================ Code ======================================
+
+
+const swiperLogo = new Swiper('.swiper__up-inner',  {
+    modules: [Navigation],
+    spaceBetween: 64,
+    slidesPerView: 6,
+    navigation:{
+        nextEl:'.swiper__up-botton-next',
+        prevEl:'.swiper__up-botton-prev',
+    },
+    
+  });
+const swiperNews = new Swiper('.swiper__down-inner',  {
+    modules: [Navigation],
+    // spaceBetween: 30,
+    // slidesPerView: 6,
+    navigation:{
+        nextEl:'.swiper__down-botton-next',
+        prevEl:'.swiper__down-botton-prev',
+    },
+    
+  });
