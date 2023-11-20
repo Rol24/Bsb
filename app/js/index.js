0003a2296a4bf736c3e178dd677ec11c88203c77
Your Name <you@example.com>
2023-11-20 16:32:10 +0300

    loadeith

--- app/js/index.js
+++ app/js/index.js
@@ -6,7 +6,7 @@ import '../scss/index.scss';
 
 //js
 import $ from 'jquery';
-import Swiper,{ Navigation, Pagination } from 'swiper';
+import Swiper, { Navigation, Pagination } from 'swiper';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
@@ -23,43 +23,72 @@ import Scrollbar from 'smooth-scrollbar';
 //// ================================ Code ======================================
 
 
-const swiperLogo = new Swiper('.swiper__up-inner',  {
-    modules: [Navigation],
-    spaceBetween: 45,
-    slidesPerView: 3,
-    navigation:{
-        nextEl:'.swiper__up-button-next',
-        prevEl:'.swiper__up-button-prev',
+const swiperLogo = new Swiper('.swiper__up-inner', {
+  modules: [Navigation],
+  spaceBetween: 45,
+  slidesPerView: 3.5,
+  navigation: {
+    nextEl: '.swiper__up-button-next',
+    prevEl: '.swiper__up-button-prev',
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 4,
+      spaceBetween: 75
     },
-    breakpoints: {
-      768: {
-        slidesPerView: 6,
-        spaceBetween: 75
-      },
-      576: {
-        slidesPerView: 2,
-        spaceBetween: 64
-      },
-    }
-    
-  });
-const swiperNews = new Swiper('.swiper__down-inner',  {
-    modules: [Navigation],
-    spaceBetween: 40,
-    slidesPerView: 2,
-    navigation:{
-        nextEl:'.swiper__down-button-next',
-        prevEl:'.swiper__down-button-prev',
+    920: {
+      slidesPerView: 5,
+      spaceBetween: 64
     },
-    breakpoints: {
-      768: {
-        slidesPerView: 3,
-        spaceBetween: 30
-      },
-      576: {
-        slidesPerView: 1,
-        spaceBetween: 30
-      }
+    576: {
+      slidesPerView: 3,
+      spaceBetween: 64
+    },
+    1024: {
+      slidesPerView: 6,
+      spaceBetween: 64
+    },
+  }
+
+});
+const swiperNews = new Swiper('.swiper__down-inner', {
+  modules: [Navigation],
+  spaceBetween: 40,
+  slidesPerView: 1.5,
+  navigation: {
+    nextEl: '.swiper__down-button-next',
+    prevEl: '.swiper__down-button-prev',
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 30
+    },
+    576: {
+      slidesPerView: 2,
+      spaceBetween: 30
     }
-    
-  });
+  }
+
+});
+
+const boxes = Array.from(document.querySelectorAll('.box'));
+
+boxes.forEach((box) => {
+  box.addEventListener("click", boxHandler);
+  console.log("box");
+});
+
+
+function boxHandler(e) {
+  e.preventDefault();
+  let currentBox = e.target.closest('.box');
+  let currentContent = e.target.nextElementSiblng;
+  currentBox.classList.toggle("active");
+  if (currentBox.classList.contains("active")){
+    // currentContent.style.maxHeight = currentContent.scrollHeight + "150px";
+  } else {
+    // currentContent.style.maxHeight = 0
+  }
+};
+
