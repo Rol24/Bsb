75730fb6d1cf54b562fa3537ab08bc3bea571441
Your Name <you@example.com>
2023-11-29 16:34:01 +0300

    load

--- app/js/index.js
+++ app/js/index.js
@@ -107,15 +107,79 @@ const burger = document.querySelector('#burger');
 const openBurger = document.querySelector('#openBurger');
 const burgerBody = document.body;
 const burgerMenu = document.querySelector('.burger-menu');
+const burgerSearch = document.querySelector('.burger-input')
 
 openBurger.addEventListener('click', () => {
   openBurger.classList.toggle('active');
   burger.classList.toggle('burger-menu--active');
   if (burgerMenu.classList.contains('burger-menu--active')) {
     burgerBody.classList.add('bodyHidden');
+    burgerSearch.style.opacity = "1";
+    console.log(burgerSearch);
   } else {
     burgerBody.classList.remove('bodyHidden');
+    burgerSearch.style.opacity = "0";
+    console.log(burgerSearch);
   }
+});
+
+const elem = document.querySelector('.tools__inner-card--image--cloud ');
+const elemEye = document.querySelector('.tools__inner-card--image--eye ');
+let mas = [elem,elemEye]
+
+let cloud = document.querySelector('#cloudCursor'); // выбираем элемент, на котором будем отслеживать движение мыши
+let cloudClass 
+
+// elemEye.addEventListener('mousemove', function (event) {
+//   const x = event.clientX; // получаем координату X мыши
+//   const y = event.clientY;  
+
+//   console.log(x,y);
+// })
+
+elem.addEventListener('mousemove', function (event) {                    
+  let yTop = elem.getBoundingClientRect().top;
+  let yBottom = elem.getBoundingClientRect().bottom;
+  let yCenter = (yTop + yBottom) / 2;
+  let xLeft = elem.getBoundingClientRect().left;
+  let xRight = elem.getBoundingClientRect().right;
+  let xCenter = (xLeft + xRight) / 2;
+  const x = event.clientX; // получаем координату X мыши
+  const y = event.clientY; // получаем координату Y мыши
+
+  //левый верхний
+  if (yTop < y && y < yCenter && xLeft < x && x < xCenter) {
+    cloud.classList.add('lv');
+    cloudClass = "lv"
+  } else {
+    cloud.classList.remove('lv');
+  }
+  //правый верхний
+  if (yTop < y && y < yCenter && xRight > x && x > xCenter) {
+    cloud.classList.add('pv');
+    cloudClass = "pv"
+  } else {
+    cloud.classList.remove('pv');
+  }
+  //правый нижний
+  if (yBottom > y && y > yCenter && xRight > x && x > xCenter) {
+    cloud.classList.add('pn');
+    cloudClass = "pn"
+  } else {
+    cloud.classList.remove('pn');
+  }
+  //левый нижний
+  if (yBottom > y && y > yCenter && xLeft < x && x < xCenter) {
+    cloud.classList.add('ln');
+    cloudClass = "ln"
+  } else {
+    cloud.classList.remove('ln');
+  }
+});
+elem.addEventListener('mouseleave', function (event) {
+  console.log("out");
+  cloud.classList.remove(cloudClass)
+
 });
 
 // let deg = 0;
